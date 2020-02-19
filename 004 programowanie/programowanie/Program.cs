833687df7412712c4597fafcc10ed81776f90013
lukaszczykmateusz <lukaszczykmateusz@105-11>
2020-02-19 10:52:55 +0100

    zadanie 004 argumenty

---
+++ 004 programowanie/programowanie/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programowanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("No witam");
+            Console.WriteLine(args[0]);
+            Console.ReadKey();
+        }
+    }
+}
