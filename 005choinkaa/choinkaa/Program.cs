d3b8b959bc98c2e52f60af2d38be691353208530
lukaszczyk.m@zshtpl.onmicrosoft.com <lukaszczyk.m@zshtpl.onmicrosoft.com>
2020-02-21 22:15:14 +0100

    005 argumenty choinka

---
+++ 005choinkaa/choinkaa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace choinkaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("          " + args[0]);
+            Console.WriteLine("         " + args[0] + args[0] + args[0]);
+            Console.WriteLine("        " + args[0] + args[0] + args[0] + args[0] + args[0]);
+            Console.WriteLine("       " + args[0] + args[0] + args[0] + args[0] + args[0] + args[0] + args[0]);
+            Console.WriteLine("      " + args[0] + args[0] + args[0] + args[0] + args[0] + args[0] + args[0] + args[0] + args[0]);
+            Console.WriteLine("     " + args[0] + args[0] + args[0] + args[0] + args[0] + args[0] + args[0] + args[0] + args[0] + args[0] + args[0]);
+            Console.WriteLine("          " + args[1]);
+            Console.ReadKey();
+        }
+    }
+}
