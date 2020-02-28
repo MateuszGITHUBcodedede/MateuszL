70befebab5cc118e9083b69ba5623d11ea0a43c7
lukaszczyk.m@zshtpl.onmicrosoft.com <lukaszczyk.m@zshtpl.onmicrosoft.com>
2020-02-28 17:50:50 +0100

    006 programowanie

---
+++ 006Rozmiarytypow/Rozmiarytypow/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozmiarytypow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Rozmiar bool: " + sizeof(bool) + " bajt");
+            Console.WriteLine("");
+            Console.WriteLine(" Rozmiar byte: " + sizeof(byte) + " bajt");
+            Console.WriteLine("");
+            Console.WriteLine(" Rozmiar sbyte: " + sizeof(sbyte) + " bajt");
+            Console.WriteLine("");
+            Console.WriteLine(" Rozmiar char: " + sizeof(char) + " bajtow");
+            Console.WriteLine("");
+            Console.WriteLine(" Rozmiar short: " + sizeof(short) + " bajtow");
+            Console.WriteLine("");
+            Console.WriteLine(" Rozmiar ushort: " + sizeof(ushort) + " bajtow");
+            Console.WriteLine("");
+            Console.WriteLine(" Rozmiar int: " + sizeof(int) + " bajtow");
+            Console.WriteLine("");
+            Console.WriteLine(" Rozmiar uint: " + sizeof(uint) + " bajtow");
+            Console.WriteLine("");
+            Console.WriteLine(" Rozmiar long: " + sizeof(long) + " bajtow");
+            Console.WriteLine("");
+            Console.WriteLine(" Rozmiar ulong: " + sizeof(ulong) + " bajtow");
+            Console.WriteLine("");
+            Console.WriteLine(" Rozmiar float: " + sizeof(float) + " bajtow");
+            Console.WriteLine("");
+            Console.WriteLine(" Rozmiar double: " + sizeof(double) + " bajtow");
+            Console.WriteLine("");
+            Console.WriteLine(" Rozmiar decimal: " + sizeof(decimal) + " bajtow");
+            Console.ReadKey();
+        }
+    }
+}
