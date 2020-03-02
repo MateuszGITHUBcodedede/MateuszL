f20f40ed2d969f1f15ca2a894d2f13aa8b95e6e3
lukaszczyk.m@zshtpl.onmicrosoft.com <lukaszczyk.m@zshtpl.onmicrosoft.com>
2020-03-02 19:16:15 +0100

    007

---
+++ 007literaly/literaly/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace literaly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" 493L typ long");
+            Console.WriteLine("");
+            Console.WriteLine(" false typ bool");
+            Console.WriteLine("");
+            Console.WriteLine(" 3.7 typ double");
+            Console.WriteLine("");
+            Console.WriteLine(" 0x3F typ int");
+            Console.WriteLine("");
+            Console.WriteLine(" 'B' typ char");
+            Console.WriteLine("");
+            Console.WriteLine(" 0xF7FF typ int");
+            Console.WriteLine("");
+            Console.WriteLine(" 07246 typ int");
+            Console.WriteLine("");
+            Console.WriteLine(" 1.2e5f typ float");
+            Console.WriteLine("");
+            Console.WriteLine(" '\u0042' typ char");
+            Console.WriteLine("");
+            Console.WriteLine(" -178609 typ int");
+            Console.ReadKey();
+        }
+    }
+}
