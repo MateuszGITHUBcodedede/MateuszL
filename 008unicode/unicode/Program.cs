0af86d33df04fccb9e6bdff7a7d7157c0df0daca
lukaszczyk.m@zshtpl.onmicrosoft.com <lukaszczyk.m@zshtpl.onmicrosoft.com>
2020-03-02 19:38:26 +0100

    008 programowanie

---
+++ 008unicode/unicode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unicode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("      \u004D\u0041\u0054\u0045\u0055\u0053\u005A \u004C\u0055\u004B\u0041\u0053\u005A\u0043\u005A\u0059\u004B");
+            Console.ReadKey();
+        }
+    }
+}
